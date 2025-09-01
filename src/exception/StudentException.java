82f64c7904bbf09feb279e7d4b4acfc0fca64c25
Do-yoon Kim <100352835+doyooning@users.noreply.github.com>
2025-09-01 10:28:36 +0900

    250901 project upload

---
+++ src/exception/StudentException.java
@@ -0,0 +1,12 @@
+package exception;
+
+import TeamMission1.common.ErrorCode;
+
+public class StudentException extends RuntimeException {
+    private final ErrorCode error;
+
+    public StudentException(ErrorCode error) {
+        super(error.getMsg());
+        this.error = error;
+    }
+}
